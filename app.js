fc71ab97edf0d8aa9b86580efb32f20b6cc02ceb
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-12 14:55:25 -0600

    Styling the candy container

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ axios.request({
         candy_container.insertAdjacentHTML(`beforeend`,
         `
             <div>
-                <h2>${success[i][0]}</h2>
-                <img src=${success[i][1]} />
-                <p>${success[i][2]}</p>
-                <button id="${success[i][0]}">Delete</button
+                <h2>${success[`data`][i][0]}</h2>
+                <img src=${success[`data`][i][1]} />
+                <p>${success[`data`][i][2]}</p>
+                <button id="${success[`data`][i][0]}">Delete</button
             </div>
         `)
     }
